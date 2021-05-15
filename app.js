cb85ff40afe0deb62f1a3551759619d9ebdc4c7d
Swaraj Pradhan <tts312@outlook.com>
2021-05-15 13:31:50 +0530

    templating for blog posts

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 const ejs= require("ejs");
 app.set('view engine', 'ejs');
+const _ = require("lodash");
 
 const homeStr= "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
 const aboutStr= "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.";
@@ -45,8 +46,23 @@ app.get("/compose",function (req,resp) {
 app.post("/compose",function (req,resp) {
     const newPost = {
         title: req.body.postTitle,
-        body: req.body.postBody,
+        content: req.body.postContent,
     };
     posts.push(newPost);
     resp.redirect("/");
+});
+
+app.get("/posts/:postName",function (req,resp) {
+    // console.log(req.params);
+    let b=-1;
+    for(let i=0;i<posts.length;i++) {
+        if(_.lowerCase(posts[i].title) === _.lowerCase(req.params.postName)) {
+            b=i; break;
+        }
+    }
+    if(b!==-1) {
+        resp.render("post.ejs",{
+            post: posts[b]
+        });
+    } else resp.redirect("/");
 });
